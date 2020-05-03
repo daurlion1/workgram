83811c398eb10a3ad98e1554e1b300be09232b44
daur <daur-lion@mail.ru>
2020-05-03 21:51:37 +0600

    Home page changed

--- resources/views/admin/layouts/parts/sidebar.blade.php
+++ resources/views/admin/layouts/parts/sidebar.blade.php
@@ -8,31 +8,31 @@
     </a></div>
 <div class="sidebar-wrapper">
     <ul class="nav">
-        <li class="nav-item active  ">
+        <li class="nav-item {{{ (Request::is('home') ? 'active' : '') }}} ">
             <a class="nav-link" href="{{route('home')}}">
                 <i class="material-icons">dashboard</i>
                 <p>Главное</p>
             </a>
         </li>
-        <li class="nav-item ">
+        <li class="nav-item {{{ (Request::is('users') ? 'active' : '') }}} ">
             <a class="nav-link" href="{{route('user.index')}}">
                 <i class="material-icons">face</i>
                 <p>Пользователи</p>
             </a>
         </li>
-        <li class="nav-item ">
+        <li class="nav-item {{{ (Request::is('categories') ? 'active' : '') }}}">
             <a class="nav-link" href="{{route('category.index')}}">
                 <i class="material-icons">bubble_chart</i>
                 <p>Категории</p>
             </a>
         </li>
-        <li class="nav-item ">
+        <li class="nav-item {{{ (Request::is('cities') ? 'active' : '') }}}">
             <a class="nav-link" href="{{route('city.index')}}">
                 <i class="material-icons">map</i>
                 <p>Города</p>
             </a>
         </li>
-        <li class="nav-item ">
+        <li class="nav-item {{{ (Request::is('projects') ? 'active' : '') }}}">
             <a class="nav-link" href="{{route('project.index')}}">
                 <i class="material-icons">gift</i>
                 <p>Проекты</p>
