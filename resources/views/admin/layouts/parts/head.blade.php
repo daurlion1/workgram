c3de02cd3e98269e04637f089d90074d592f93b8
daur <daur-lion@mail.ru>
2020-05-03 18:50:19 +0600

    icon changed color for head of table title changed

--- resources/views/admin/layouts/parts/head.blade.php
+++ resources/views/admin/layouts/parts/head.blade.php
@@ -1,9 +1,9 @@
 <meta charset="utf-8" />
-<link rel="apple-touch-icon" sizes="76x76" href="{{asset('admin/img/apple-icon.png')}}">
-<link rel="icon" type="image/png" href="{{asset('admin/img/favicon.png')}}">
+{{--<link rel="apple-touch-icon" sizes="76x76" href="{{asset('admin/img/apple-icon.png')}}">--}}
+<link rel="icon" type="image/png" href="{{asset('admin/img/favicon.jpg')}}">
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 <title>
-    Material Dashboard by Creative Tim
+    WorkGram
 </title>
 <meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
 <!--     Fonts and icons     -->
