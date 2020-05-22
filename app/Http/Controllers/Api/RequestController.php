64224fc68b3f3e452ebeef2f3bd2add039d2c452
daur <daur-lion@mail.ru>
2020-05-22 18:03:48 +0600

    user cannot make request to their project

--- app/Http/Controllers/Api/RequestController.php
+++ app/Http/Controllers/Api/RequestController.php
@@ -27,22 +27,33 @@ class RequestController extends ApiBaseController
             'errorCode' => ErrorCode::RESOURCE_NOT_FOUND
         ]);
 
-        if($is_to_specific_user == false) {
+        if($user->id != $project->creator_id) {
+
+            if ($is_to_specific_user == false) {
 
                 ProjectRequest::create([
                     'user_id' => $user->id,
                     'is_to_specific_user' => $is_to_specific_user,
                     'project_id' => $project_id
                 ]);
-            $message = "Упешно отправлен";
+                $message = "Упешно отправлен";
+            } else {
+                ProjectRequest::create([
+                    'user_id' => $request->implementer_id,
+                    'is_to_specific_user' => $is_to_specific_user,
+                    'project_id' => $project_id
+                ]);
+                $message = "Упешно отправлен";
+            }
         }
         else{
-            ProjectRequest::create([
-                'user_id' => $request->implementer_id,
-                'is_to_specific_user' => $is_to_specific_user,
-                'project_id' => $project_id
+            throw new ApiServiceException(403, false, [
+                'errors' => [
+                    'Вы не можете стать исполнителем своего проекта!'
+                ],
+                'errorCode' => ErrorCode::RESOURCE_NOT_FOUND
             ]);
-            $message = "Упешно отправлен";
+
         }
         return $this->successResponse(['message' => $message]);
 
